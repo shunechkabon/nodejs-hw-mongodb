674c181d71fc1c89c7000bf19c1ab6382ab60113
shunechkabon <bondaruk.aleksandra92@gmail.com>
2024-09-26 21:05:01 +0100

    add validation, pagination and filters

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,41 @@
 import { ContactsCollection } from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContacts = async () => {
-    const contacts = await ContactsCollection.find();
-    return contacts;
+export const getAllContacts = async ({
+    page = 1,
+    perPage = 10,
+    sortOrder = SORT_ORDER.ASC,
+    sortBy = '_id',
+    filters = {},
+}) => {
+    const limit = perPage;
+    const skip = (page - 1) * perPage;
+    
+    const contactsQuery = ContactsCollection.find();
+
+    if (filters.contactType) {
+        contactsQuery.where('contactType').equals(filters.contactType);
+    }
+    if (filters.isFavourite !== undefined) {
+        contactsQuery.where('isFavourite').equals(filters.isFavourite);
+    }
+
+    const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+        contactsQuery
+            .skip(skip)
+            .limit(limit)
+            .sort({ [sortBy]: sortOrder })
+            .exec(),
+    ]);
+    
+    const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+    return {
+        data: contacts,
+        ...paginationData,
+    };
 };
 
 export const getContactById = async (contactId) => {
